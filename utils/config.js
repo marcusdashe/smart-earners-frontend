9a3c6c20730dc8482168d01474fdea432e67ef55
inedumozey <inedumozey@gmail.com>
2022-07-21 05:01:12 -0700

    backend base url added

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,3 @@
-export const BACKEND_BASE_URL = "http://localhost:5000"
+// export const BACKEND_BASE_URL = "http://localhost:5000"
+export const BACKEND_BASE_URL = "https://seal-app-uuoo5.ondigitalocean.app"
 export const FRONTEND_BASE_URL = "http://localhost:3000"
